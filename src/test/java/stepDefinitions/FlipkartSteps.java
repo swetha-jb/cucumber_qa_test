c2ce968ae55db49bfcee69e8d26333ecb92470a4
swetha <swetha.j@vvdntech.in>
2025-07-23 15:57:31 +0530

    test

--- src/test/java/stepDefinitions/FlipkartSteps.java
+++ src/test/java/stepDefinitions/FlipkartSteps.java
@@ -14,10 +14,17 @@ public class FlipkartSteps {
 
     @Given("user is on Flipkart homepage")
     public void user_is_on_flipkart_homepage() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.flipkart.com/");
-        loginPage = new FlipkartLoginPage(driver);
+         ChromeOptions options = new ChromeOptions();
+   	 options.addArguments("--headless=new");
+   	 options.addArguments("--no-sandbox");
+   	 options.addArguments("--disable-dev-shm-usage");
+   	 options.addArguments("--disable-gpu");
+   	 options.addArguments("--remote-debugging-port=9222");
+
+   	 driver = new ChromeDriver(options);
+   	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+   	 driver.get("https://www.flipkart.com/");
+   	 loginPage = new FlipkartLoginPage(driver);
     }
 
     @When("user closes the login popup")
